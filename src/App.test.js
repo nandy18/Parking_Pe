01e12a52720547f3639542e7ea1b9f9ee99d3a33
Nandish <nandy.aggarwal@gmail.com>
2023-11-17 09:37:31 -0600

    update

--- src/App.test.js
+++ src/App.test.js
@@ -5,10 +5,5 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText('Parking Pe');
   expect(linkElement).toBeInTheDocument();
-  const linkElement1 = screen.getByText('VIP Parking');
-  expect(linkElement1).toBeInTheDocument();
-  const linkElement2= screen.getByText('Reserve Parking');
-  expect(linkElement2).toBeInTheDocument();
-  const linkElement3 = screen.getByText('Normal Parking');
-  expect(linkElement3).toBeInTheDocument();
+
 });
