a52fb4984961d71a1b50691605ec3c3a05510c1b
Nandish <nandy.aggarwal@gmail.com>
2023-11-16 21:28:28 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [parkingHours, setParkingHours] = useState(1)
+
+  /*const [parkingHoursVip, setParkingHoursVip] = useState(1)
+  const [parkingHoursReserved, setParkingHoursReserved] = useState(1)
+  const [parkingHoursNormal1, setParkingHoursNormal1] = useState(1)
+  const [parkingHoursNormal2, setParkingHoursNormal2] = useState(1)*/
+
+  const parkingCategoryHours = {
+    'vip': 5.00,
+    'reserved': 0.00,
+    'normal': 3.00
+  }
+
+  const OnInputValueChanged = (e) => {
+    setParkingHours(e.target.value)
+
+  }
+
+  const OnParkingAmountPay = (category) => {
+    console.log(parkingHours);
+    console.log(category);
+
+    const parkAmount = parkingCategoryHours[category];
+    let totalParkingAmount = parseInt(parkingHours) * parseFloat(parkAmount);
+    console.log(totalParkingAmount)
+    alert( "Thank You For Paying $" + totalParkingAmount)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          ParkingPe
-        </h1>
-        <h3>(under construction)</h3>
-        
-     
-      </header>
+    <div className='app-container'>
+      <div className='main-container'>
+        <h1>Parking Pe</h1>
+        <div className='parking-container'>
+          <section className='vip-section section'>
+            <h2 className='title'>VIP Parking</h2>
+            <div className='input-grpup'>
+              <label htmlFor='vip'>How many hours you want to park?</label>
+              <input className='input-text' type="number" placeholder="How many hours you want to park?" name='vip' id='vip' value={parkingHours} onChange={(e) => OnInputValueChanged(e)} />
+            </div>
+            <button className='btn' onClick={(e) => OnParkingAmountPay("vip")}>PAY</button>
+
+          </section>
+          <section className='reserved-section section'>
+          <h2 className='title'>Reserved Parking</h2>
+            <div className='input-grpup'>
+              <label htmlFor='reserved'>How many hours you want to park?</label>
+              <input className='input-text' type="number" placeholder="How many hours you want to park?" name='reserved' id='reserved' value={parkingHours} onChange={(e) => OnInputValueChanged(e)} />
+            </div>
+            <button className='btn' onClick={(e) => OnParkingAmountPay("reserved")}>PAY</button>
+          </section>
+          <section className='normal-section section'>
+          <h2 className='title'>Normal Parking</h2>
+            <div className='input-grpup'>
+              <label>How many hours you want to park?</label>
+              <input className='input-text' type="number" placeholder="How many hours you want to park?" />
+            </div>
+            <button className='btn'>PAY</button>
+          </section>
+          <section className='normal-section section'>
+          <h2 className='title'>Normal Parking</h2>
+            <div className='input-grpup'>
+              <label>How many hours you want to park?</label>
+              <input className='input-text' type="number" placeholder="How many hours you want to park?" />
+            </div>
+            <button className='btn'>PAY</button>
+          </section>
+        </div>
+      </div>
+      
+
     </div>
   );
 }
